66dda0c2de67faeff6c95aa6b36a2c0796228863
masx200 <masx200@qq.com>
2020-08-30 22:00:27 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { retryfetch } from "./retryfetch.js";
 import { start } from "./start.js";
 export const fetch = retryfetch;
 // @ts-ignore
-const rpcparam = new URL(import.meta.url).searchParams.get("rpc");
+const rpcparam = new URL(import.meta.url).searchParams.get("rpcurl");
 const rpcurl = rpcparam ?? "http://localhost:6800/jsonrpc";
 checkurl(rpcurl);
 export { rpcurl };
