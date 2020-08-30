df697c2d5f8e6e5b1eeaf2ab70ce7bab720dd007
masx200 <masx200@qq.com>
2020-08-30 22:07:23 +0800

    rpcurl

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import { retryfetch } from "./retryfetch.js";
 import { start } from "./start.js";
 export const fetch = retryfetch;
 // @ts-ignore
-const rpcparam = new URL(import.meta.url).searchParams.get("rpcurl");
+const params = new URL(import.meta.url).searchParams;
+// @ts-ignore
+console.log(Object.fromEntries(params));
+// @ts-ignore
+const rpcparam = parms.get("rpcurl");
+console.log(rpcparam);
 const rpcurl = rpcparam ?? "http://localhost:6800/jsonrpc";
 checkurl(rpcurl);
 export { rpcurl };
