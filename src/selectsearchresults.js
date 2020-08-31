f759b0f3b81812a83922b057f5dd12da0dd80881
masx200 <masx200@qq.com>
2020-08-31 11:22:47 +0800

    小小的优化

--- src/selectsearchresults.js
+++ src/selectsearchresults.js
@@ -1,5 +1,12 @@
 export function selectsearchresults(document) {
-    return Array.from(new Set(Array.from(new Set([...document.querySelectorAll(".node  .title1 a"),...document.querySelectorAll(".node  .title a")])).map(
-        (a) => a.href
-    )));
+    return Array.from(
+        new Set(
+            Array.from(
+                new Set([
+                    ...document.querySelectorAll(".node  .title1 a"),
+                    ...document.querySelectorAll(".node  .title a"),
+                ])
+            ).map((a) => a.href)
+        )
+    );
 }
